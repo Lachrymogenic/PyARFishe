49fa2cc69bf5f77b61a257a170b8337612e084ac
Lacrimogenic <liamdoubleday@gmail.com>
2021-09-04 00:01:26 +0100

    Improved functionality and configuration
    Added a button that allows you to configure your Detection Field, for manual adjustment. Fixed bug where pressing Q sometimes didnt close the process.

--- main.py
+++ main.py
@@ -25,15 +25,13 @@ def show(key):
         global quitloop
         quitloop = 1
         return False
-    else:
-        return False
   
 #mon = {'top': 480, 'left': 900, 'width': 100, 'height': 80}
 mon = {'top': 470, 'left': 930, 'width': 50, 'height': 50}
 with mss.mss() as sct:
     root = Tk()
     root.title("PyARfishe")
-    root.geometry('350x300')
+    root.geometry('350x270')
     root.resizable(0,0)
     frame = Frame(root)
     frame.pack()
@@ -109,13 +107,55 @@ with mss.mss() as sct:
             
     btn = Button(root, text = "Start", command=startfishe)
     btn.pack()
+    
+    def DSave(top,left,width,height):
+        global mon
+        mon = {'top': top, 'left': left, 'width': width, 'height': height}
+        print(mon)
+    
+    def DField():
+        window = Toplevel()
+        window.geometry('350x120')
+        window.resizable(0,0)
+        newlabel = Label(window, text = "Detection Field Configuration")
+        newlabel.pack()
+        newframe = Frame(window)
+        newframe.pack()
+        newframe2 = Frame(window)
+        newframe2.pack()
+        newframe3 = Frame(window)
+        newframe3.pack()
+        
+        nentry = Entry(newframe, width = 10)
+        nentry.pack(side=LEFT)
+        nentry2 = Entry(newframe, width = 10)
+        nentry2.pack(side=LEFT)
+        nentry3 = Entry(newframe, width = 10)
+        nentry3.pack(side=LEFT)
+        nentry4 = Entry(newframe, width = 10)
+        nentry4.pack(side=LEFT)
+        
+        nentry.insert(0,'470')
+        nentry2.insert(0,'930')
+        nentry3.insert(0,'50')
+        nentry4.insert(0,'50')
+        
+        newlbl = Label(newframe2, text = "top, left, width, height")
+        newlbl.pack(side=LEFT)
+        newbtn = Button(newframe3, text = "Preview", command=startfishe)
+        newbtn.pack(side=LEFT)
+        newbtn2 = Button(newframe3, text = "Save", command=lambda: DSave(int(nentry.get()),int(nentry2.get()),int(nentry3.get()),int(nentry4.get())))
+        newbtn2.pack(side=LEFT)
+        
+    btn2 = Button(frame, text = "Detection Field", command=DField)
+    btn2.pack(side=LEFT)
     lbl2 = Label(frame3, text = "Screenshots per second")
     lbl2.pack(side=LEFT)
     entry = Entry(frame3, width = 10)
     entry.insert(0,'0.5')
     entry.pack(side=LEFT, padx=10)
     lbl3 = Label(frame2, text = "Fishing Rod Slot")
-    lbl3.pack(side=LEFT)
+    lbl3.pack(side=LEFT, padx=28)
     entry2 = Entry(frame2, width = 10)
     entry2.insert(0,'3')
     entry2.pack(side=LEFT, padx=10, pady=10)
